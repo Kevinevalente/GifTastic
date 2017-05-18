b4a707d65b176c398d8de916663c29cd48f8ce5b
Kevin Valente <Kevin Valente>
2017-05-18 13:10:57 -0700

    Added Gifs to actors!

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -23,7 +23,7 @@ for (var i = 0; i < actors.length; i++) {
 //Create buttons for each actor in array 
 var b = $("<button>");
 b.addClass("actor");
-b.attr("data-name", actors[i]);
+b.attr("data-actor", actors[i]);
 b.text(actors[i]);
 $("#actorTabs").append(b);
 	}
@@ -32,15 +32,54 @@ $("#actorTabs").append(b);
 //Event Click handler
 $("#addActor").on("click", function(event) {
 event.preventDefault();
-
-var search = $("#search-input").val().trim();
-actors.push(search);
+var actorName = $("#search-input").val().trim();
+actors.push(actorName);
 
 // Call function 
 showGifs();
       });
 //Call to show initial array of actors
 showGifs();
+
+//This is the on click function for actor button(s)
+function giphyAjaxCall() { 
+	$("button").on("click", function() {
+//setting variable actor equal to the value of "this" button
+      var actor = $(this).attr("data-actor");
+//set variable URl up so that actor is interchangable
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        actor + "&api_key=dc6zaTOxFJmzC&limit=10";
+//ajax call using get method to retrieve JSON object from API
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+//Promise that indicates AJAX call has been executed and then respond in JSON format
+        .done(function(response) {
+
+//Results is an array of objects
+ var results = response.data;
+
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+
+            $("#actors").prepend(gifDiv);
+          }
+
+        });
+     });
+};
+$(document).on('click', 'button', giphyAjaxCall);
 
 
 
